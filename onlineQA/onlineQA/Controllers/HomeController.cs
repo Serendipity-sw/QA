c446976a2fcb91795882a29be10cc1c3833042a5
gloomysw <Sw435464>
2016-05-24 22:40:51 +0800

    代码上传

---
+++ onlineQA/onlineQA/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace onlineQA.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ onlineQA/onlineQA/control/control.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace onlineQA.control
+{
+    public class control : Controller
+    {
+
+    }
+}
